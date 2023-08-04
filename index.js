53ba8d09d0b205d6c1a1486469adf2d04d115f16
Connor <connordbrown@gmail.com>
2023-08-04 17:07:38 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData (userName, userEmail) {
+    
+    const formData = {
+        name: `${userName}`,
+        email: `${userEmail}`
+    };
+
+    const postObject = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", postObject)
+      .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        const x = document.createElement('p');
+        x.textContent = object.id;
+        document.body.append(x);
+    })
+    .catch(function (error) {
+        alert("Error!");
+        const x = document.createElement('p');
+        x.textContent = error.message;
+        document.body.append(x);
+    })
+
+}
+
